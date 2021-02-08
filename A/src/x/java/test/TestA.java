95b6418c6f815aa52b552b16702e8d3812d6abd6
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2021-02-08 19:22:46 +0800

    Initial commit

---
+++ A/src/x/java/test/TestA.java
@@ -0,0 +1,5 @@
+package test;
+
+public class TestA {
+    public static void test() {}
+}
---
+++ B/src/main/java/test/TestB.java
@@ -0,0 +1,9 @@
+package test;
+
+import test.TestA;
+
+public class TestB {
+    public static void main(String[] args) {
+        TestA.test();
+    }
+}
